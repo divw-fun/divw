af7554e7b55dca1e5a5389a353e610259e775df1
divw-fun <192427325+divw-fun@users.noreply.github.com>
2025-12-22 16:41:19 +0000

    feat(sdk): create index exports

---
+++ sdk/src/index.ts
@@ -0,0 +1,9 @@
+export { DivwClient } from "./client";
+export { DiveStatus } from "./types";
+export type { DiveOptions, DiveResult, ProtocolStats } from "./types";
+export {
+  calculateRecommendedWire,
+  formatDepth,
+  isValidDepth,
+  isValidWireLength,
+} from "./utils";
