dccd7b943f3594750cc1c8564728e324cb589e3c
divw-fun <192427325+divw-fun@users.noreply.github.com>
2026-01-05 14:36:58 +0000

    refactor(sdk): improve type exports

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,6 +1,7 @@
 export { DivwClient } from "./client";
 export { DiveStatus } from "./types";
 export type { DiveOptions, DiveResult, ProtocolStats } from "./types";
+export * from "./constants";
 export {
   calculateRecommendedWire,
   formatDepth,
