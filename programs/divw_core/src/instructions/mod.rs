1c63a38e3278bb481dc3fe78e49074658ca8d446
divw-fun <192427325+divw-fun@users.noreply.github.com>
2026-01-08 19:57:48 +0000

    refactor(core): consolidate admin instructions

--- programs/divw_core/src/instructions/mod.rs
+++ programs/divw_core/src/instructions/mod.rs
@@ -1,9 +1,13 @@
-pub mod initialize;
-pub mod create_dive;
-pub mod spool_up;
 pub mod abort_dive;
+pub mod admin;
+pub mod create_dive;
+pub mod initialize;
+pub mod spool_up;
+pub mod transfer_authority;
 
-pub use initialize::*;
-pub use create_dive::*;
-pub use spool_up::*;
 pub use abort_dive::*;
+pub use admin::*;
+pub use create_dive::*;
+pub use initialize::*;
+pub use spool_up::*;
+pub use transfer_authority::*;
